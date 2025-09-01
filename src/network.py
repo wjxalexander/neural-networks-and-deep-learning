555e988dc5272fdab12a9163d87c3f9aba27b55e
jingxinwang <wjxalexander@hotmail.com>
2025-09-02 02:53:42 +0800

    network to py3

--- src/network.py
+++ src/network.py
@@ -12,9 +12,11 @@ and omits many desirable features.
 #### Libraries
 # Standard library
 import random
+import time
 
 # Third-party libraries
 import numpy as np
+
 
 class Network(object):
 
@@ -31,7 +33,18 @@ class Network(object):
         ever used in computing the outputs from later layers."""
         self.num_layers = len(sizes)
         self.sizes = sizes
+        """
+        1. 为什么是 sizes[1:] 而不是 sizes？
+        输入层（sizes[0]）没有 bias（偏置）。
+        因为输入层只是外部数据输入，不需要额外的偏移量。偏置只作用在隐藏层和输出层。所以从 sizes[1:] 开始，逐层为这些层的神经元生成偏置向量。
+        """
         self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
+        """
+        这行代码的意思是：在类初始化时，生成一个神经网络每一层的 权重矩阵，并用正态分布随机初始化。
+        zip([2, 3], [3, 1]) → [(2, 3), (3, 1)]
+        第一个矩阵形状是 (3, 2)：3 行：隐藏层有 3 个神经元2 列：每个神经元连接输入层的 2 个输入
+        第二个矩阵形状是 (1, 3)：1 行：输出层有 1 个神经元3 列：这个神经元连接隐藏层的 3 个输出
+        """
         self.weights = [np.random.randn(y, x)
                         for x, y in zip(sizes[:-1], sizes[1:])]
 
@@ -53,18 +66,20 @@ class Network(object):
         tracking progress, but slows things down substantially."""
         if test_data: n_test = len(test_data)
         n = len(training_data)
-        for j in xrange(epochs):
+        for j in range(epochs):
+            time1 = time.time()
             random.shuffle(training_data)
             mini_batches = [
                 training_data[k:k+mini_batch_size]
-                for k in xrange(0, n, mini_batch_size)]
+                for k in range(0, n, mini_batch_size)]
             for mini_batch in mini_batches:
                 self.update_mini_batch(mini_batch, eta)
+            time2 = time.time()
             if test_data:
-                print "Epoch {0}: {1} / {2}".format(
-                    j, self.evaluate(test_data), n_test)
+                print("Epoch {0}: {1} / {2}, took {3:.2f} seconds".format(
+                    j, self.evaluate(test_data), n_test, time2-time1))
             else:
-                print "Epoch {0} complete".format(j)
+                print("Epoch {0} complete in {1:.2f} seconds".format(j, time2-time1))
 
     def update_mini_batch(self, mini_batch, eta):
         """Update the network's weights and biases by applying
@@ -73,10 +88,19 @@ class Network(object):
         is the learning rate."""
         nabla_b = [np.zeros(b.shape) for b in self.biases]
         nabla_w = [np.zeros(w.shape) for w in self.weights]
+        """
+        nabla_b / nabla_w：初始化为全 0，用来累计 整个 mini-batch 的梯度和。
+        zip(...)：把当前累计值和这次样本的梯度对应相加。
+        经过整个循环，nabla_b 和 nabla_w 就是 mini-batch 所有样本的梯度总和。
+        """
         for x, y in mini_batch:
             delta_nabla_b, delta_nabla_w = self.backprop(x, y)
             nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
             nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
+        """
+        b = b - alpha * dj_db                            
+        w = w - alpha * dj_dw    
+        """
         self.weights = [w-(eta/len(mini_batch))*nw
                         for w, nw in zip(self.weights, nabla_w)]
         self.biases = [b-(eta/len(mini_batch))*nb
@@ -109,7 +133,7 @@ class Network(object):
         # second-last layer, and so on.  It's a renumbering of the
         # scheme in the book, used here to take advantage of the fact
         # that Python can use negative indices in lists.
-        for l in xrange(2, self.num_layers):
+        for l in range(2, self.num_layers):
             z = zs[-l]
             sp = sigmoid_prime(z)
             delta = np.dot(self.weights[-l+1].transpose(), delta) * sp
@@ -127,8 +151,7 @@ class Network(object):
         return sum(int(x == y) for (x, y) in test_results)
 
     def cost_derivative(self, output_activations, y):
-        """Return the vector of partial derivatives \partial C_x /
-        \partial a for the output activations."""
+        # """Return the vector of partial derivatives \partial C_x /partial a for the output activations."""
         return (output_activations-y)
 
 #### Miscellaneous functions
@@ -139,3 +162,15 @@ def sigmoid(z):
 def sigmoid_prime(z):
     """Derivative of the sigmoid function."""
     return sigmoid(z)*(1-sigmoid(z))
+
+if __name__ == "__main__":
+    import mnist_loader
+
+    # 加载 MNIST 数据 (x, y)
+    training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+
+    # 初始化网络：784 输入（28x28像素），30 隐藏，10 输出（数字 0-9）
+    net = Network([784, 30, 10])
+    # net = Network([784, 100, 10])
+    # 训练：30 个 epoch，每个 mini-batch 10 个样本，学习率 3.0
+    net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
